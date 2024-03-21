baae6dec4f4e816d364583bad15cbabe99c5b4be
GauravSahu13 <gauravsahu7045@gmail.com>
2024-03-21 13:15:00 +0530

    Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Cat-Dog",
+    version="0.0.1",
+    author="Gaurav",
+    author_email="gauravsahu9821@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
